2b10a709dd1ab749300efdaa1c95c180c7439f40
Pavel <pavelvaloshyn89@gmail.com>
2021-10-28 19:45:09 +0200

    fix: missed bag, missed letter S in path products

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,8 @@ const API_PATHS = {
   product: 'https://b6sld3tghb.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://m85cq3m7se.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://b6sld3tghb.execute-api.eu-west-1.amazonaws.com/dev',
+  // bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'http://hellobro89-cart-api.eu-west-1.elasticbeanstalk.com/api',
 };
 
